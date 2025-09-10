5c813423643dd1485d1df3f7b607aef005a4799d
Pedro Henrique Windisch <pedrowindisch@gmail.com>
2025-09-10 11:39:08 -0300

    feat: select list parsing

--- compiler/src/main/java/br/com/windisch/xtract/compiler/Parser.java
+++ compiler/src/main/java/br/com/windisch/xtract/compiler/Parser.java
@@ -4,13 +4,18 @@ import java.util.ArrayList;
 import java.util.List;
 
 import br.com.windisch.xtract.compiler.models.DocumentType;
+import br.com.windisch.xtract.compiler.models.ExtractionMethodType;
 import br.com.windisch.xtract.compiler.models.Method;
+import br.com.windisch.xtract.compiler.models.OutputType;
 import br.com.windisch.xtract.compiler.models.ParserException;
 import br.com.windisch.xtract.compiler.models.Token;
 import br.com.windisch.xtract.compiler.models.TokenType;
 import br.com.windisch.xtract.compiler.nodes.Base;
+import br.com.windisch.xtract.compiler.nodes.Output;
+import br.com.windisch.xtract.compiler.nodes.ParseBody;
 import br.com.windisch.xtract.compiler.nodes.Program;
 import br.com.windisch.xtract.compiler.nodes.Scrape;
+import br.com.windisch.xtract.compiler.nodes.SelectField;
 
 public class Parser {
     // recursive descent parser
@@ -42,6 +47,12 @@ public class Parser {
     private boolean check(TokenType type) {
         if (isAtEnd()) return false;
         return peek().getType() == type;
+    }
+
+    private boolean check(TokenType... types) {
+        for (var type : types) if (check(type)) return true;
+
+        return false;
     }
 
     private boolean match(TokenType... types) {
@@ -112,36 +123,66 @@ public class Parser {
         return new Scrape(identifier.getLexeme(), method, url.getLexeme(), parsers);
     }
 
-    public List<br.com.windisch.xtract.compiler.nodes.Parser> parseScrapeBody() throws ParserException {
-        return null;
-    }
-
     public br.com.windisch.xtract.compiler.nodes.Parser parseParse() throws ParserException {
         if (!match(TokenType.PARSE)) throw new ParserException(peek().getPosition(), "Expected 'parse'");
 
         if (match(TokenType.HTML)) consume(TokenType.LBRACE, "Expected '{' after 'html'.");
 
-        // var body = parseParseBody();
+        var body = parseParseBody();
 
-        while (!check(TokenType.RBRACE) && !isAtEnd()) advance();
         consume(TokenType.RBRACE, "Expected '}' after parser body.");
 
-        while (!check(TokenType.RBRACE) && !isAtEnd()) advance();
-        consume(TokenType.RBRACE, "Expected '}' after parser body.");
+        Output output = null;
+        if (check(TokenType.OUTPUT)) parseOutput();
 
-        if (check(TokenType.OUTPUT)) {
-            advance();
+        return new br.com.windisch.xtract.compiler.nodes.Parser(DocumentType.HTML, body, output);
+    }
 
-            if (!match(TokenType.JSON, TokenType.CSV)) {
-                throw new ParserException(peek().getPosition(), "Expected output format (json or csv).");
-            }
+    public ParseBody parseParseBody() throws ParserException {
+        consume(TokenType.SELECT, "Expected 'select' in parser body.");
+        var selector = consume(TokenType.STRING, "Expected selector string after 'select'.");
 
-            if (match(TokenType.APPEND)) {
-            }
+        consume(TokenType.AS, "Expected 'as' after selector.");
+        var name = consume(TokenType.IDENTIFIER, "Expected identifier after 'as'.");
 
-            var fileName = consume(TokenType.STRING, "Expected output file name.");
+        consume(TokenType.LBRACE, "Expected '{' after parser declaration.");
+
+        List<SelectField> fields = new ArrayList<>();
+        while (!check(TokenType.RBRACE) && !isAtEnd()) {
+            var fieldName = consume(TokenType.IDENTIFIER, "Expected field name.");
+            consume(TokenType.COLON, "Expected ':' after field name.");
+
+            var isOptional = match(TokenType.OPTIONAL);
+
+            if (!check(TokenType.TEXT, TokenType.ATTR)) throw new ParserException(peek().getPosition(), "Expected extraction type (text or attr).");
+
+            var type = advance().getType() == TokenType.TEXT ? ExtractionMethodType.TEXT : ExtractionMethodType.ATTR;
+
+            consume(TokenType.LPAREN, "Expected '(' after extraction type.");
+
+            var attrName = check(TokenType.STRING) ? consume(TokenType.STRING, "Expected attribute or element name for 'attr' extraction type.") : null;
+
+            consume(TokenType.RPAREN, "Expected ')' after extraction method.");
+
+            fields.add(new SelectField(fieldName.getLexeme(), isOptional, type, attrName != null ? attrName.getLexeme() : null));
         }
 
-        return new br.com.windisch.xtract.compiler.nodes.Parser(DocumentType.HTML, null, null);
+        while (!check(TokenType.RBRACE) && !isAtEnd()) advance();
+        consume(TokenType.RBRACE, "Expected '}' after parser body.");
+
+        return new ParseBody(selector.getLexeme(), name.getLexeme(), fields);
+    }
+
+    public Output parseOutput() throws ParserException {
+        advance();
+
+        if (!match(TokenType.JSON, TokenType.CSV)) throw new ParserException(peek().getPosition(), "Expected output format (json or csv).");
+
+        var format = previous().getType() == TokenType.JSON ? OutputType.JSON : OutputType.CSV;
+
+        boolean append = match(TokenType.APPEND);
+        var fileName = consume(TokenType.STRING, "Expected output file name.");
+
+        return new Output(append, format, fileName.getLexeme());
     }
 }
--- compiler/src/main/java/br/com/windisch/xtract/compiler/nodes/SelectField.java
+++ compiler/src/main/java/br/com/windisch/xtract/compiler/nodes/SelectField.java
@@ -4,21 +4,27 @@ import br.com.windisch.xtract.compiler.models.ExtractionMethodType;
 
 public class SelectField extends Node {
     private String field;
+    private boolean isOptional;
     private ExtractionMethodType extractionMethodType;
     private String attributeName;
 
-    public SelectField(String field, ExtractionMethodType extractionMethodType, String attributeName) {
+    public SelectField(String field, boolean isOptional, ExtractionMethodType extractionMethodType, String attributeName) {
         this.field = field;
+        this.isOptional = isOptional;
         this.extractionMethodType = extractionMethodType;
         this.attributeName = attributeName;
     }
 
-    public SelectField(String field, ExtractionMethodType extractionMethodType) {
-        this(field, extractionMethodType, null);
+    public SelectField(String field, boolean isOptional, ExtractionMethodType extractionMethodType) {
+        this(field, isOptional, extractionMethodType, null);
     }
 
     public String getField() {
         return field;
+    }
+
+    public boolean isOptional() {
+        return isOptional;
     }
 
     public ExtractionMethodType getExtractionMethodType() {
